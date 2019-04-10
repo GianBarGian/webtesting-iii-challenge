9e8b07064a193a247c72d203dd9547965b988c87
GianBarGian <giacomobenati@hotmail.it>
2019-04-10 13:30:27 +0200

    added first test to control

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import * as rt from 'react-testing-library';
+import Controls from './Controls';
+
+describe('Control component tests', () => {
+    it('should provide buttons to toggle the closed and locked states', () => {
+        const wrapper = rt.render(<Controls locked={false} closed={false} toggleLocked={() => {}} toggleClosed={() => {}}/>);
+        expect(wrapper.getByText(/lock/i));
+        expect(wrapper.getByText(/close/i))
+    })
+})
