5b6d953638e658bf572fc90aff7a9560ebe3f453
GianBarGian <giacomobenati@hotmail.it>
2019-04-10 14:10:09 +0200

    added Dashboard tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,10 +2,20 @@ import React from 'react';
 import * as rt from 'react-testing-library';
 import Controls from './Controls';
 
+afterEach(rt.cleanup);
+
 describe('Control component tests', () => {
     it('should provide buttons to toggle the closed and locked states', () => {
-        const wrapper = rt.render(<Controls locked={false} closed={false} toggleLocked={() => {}} toggleClosed={() => {}}/>);
+        const wrapper = rt.render(<Controls />);
         expect(wrapper.getByText(/lock/i));
         expect(wrapper.getByText(/close/i))
+    })
+
+    it('should changes buttons text to reflect the state the door will be in if clicked', () => {
+        // display default value
+        // click on button 
+        // display changed value
+        const wrapper = rt.render(<Controls />);
+        rt.fireEvent.click(wrapper.getByText(/close/i));
     })
 })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import * as rt from 'react-testing-library';
+
+import Dashboard from './Dashboard';
+import { isMainThread } from 'worker_threads';
+
+describe('Dashboard component', () => {
+    it('display Display component', () => {
+        const wrap = rt.render(<Dashboard />);
+        expect(wrap.getByText(/open/i))
+        expect(wrap.getByText(/unlocked/i))
+    })
+
+    it('display Controls component', () => {
+        const wrap = rt.render(<Dashboard />);
+        expect(wrap.getByText(/clos/i))
+        expect(wrap.getByText(/lock/i))
+    })
+})
